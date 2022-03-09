bdab95c1debadb653f1b0dc5278af95f7be26679
deedeeh <dinahafez.8@gmail.com>
2022-03-09 18:16:33 +0000

    Implemented typescript for roman_numerals kata without tests

--- src/roman_numerals.js
+++ src/roman_numerals.js
@@ -1,39 +1,20 @@
-/* The Kata says you should write a function to convert from normal numbers to Roman Numerals: eg
-1 --> I
-4 --> IV
-5 --> V
-9 --> IX
-10 --> X
-40 --> XL
-50 --> L
-90 --> XC
-100 --> C
-400 --> CD
-500 --> D
-900 --> CM
-1000 --> M
---
-7 --> VII
-There is no need to be able to convert numbers larger than about 3000. (The Romans themselves didn’t tend to go any higher)
-*/
-
-//create an object
-//iterate 
-//while loop
-//create a string variable to hold the result
-
-export const numberToRomanNumerals = number => {
-  if(number === undefined) throw new Error("number is required");
-  if(typeof number !== 'number') throw new Error("A Number type is required");
-  if(number < 0) throw new Error("A positive number is required");
-  let romanNumerals = "";
-  const numbersAndRomans = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1};
-  if(number > 3000) return romanNumerals;
-  Object.keys(numbersAndRomans).forEach(key => {
-    while(number >= numbersAndRomans[key]) {
-      romanNumerals += key
-      number -= numbersAndRomans[key]
-    }
-  })
-  return romanNumerals;
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.numToRomanNumerals = void 0;
+const numToRomanNumerals = (num) => {
+    if (num < 0)
+        throw new Error("A positive num is required");
+    let romanNumerals = "";
+    const numsAndRomans = { "M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1 };
+    const getKeys = Object.keys;
+    if (num > 3000)
+        return romanNumerals;
+    getKeys(numsAndRomans).forEach(key => {
+        while (num >= numsAndRomans[key]) {
+            romanNumerals += key;
+            num -= numsAndRomans[key];
+        }
+    });
+    return romanNumerals;
+};
+exports.numToRomanNumerals = numToRomanNumerals;
---
+++ src/roman_numerals.ts
@@ -0,0 +1,14 @@
+export const numToRomanNumerals = (num: number): string => {
+  if(num < 0) throw new Error("A positive num is required");
+  let romanNumerals = "";
+  const numsAndRomans = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1};
+  const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
+  if(num > 3000) return romanNumerals;
+  getKeys(numsAndRomans).forEach(key => {
+    while(num >= numsAndRomans[key]) {
+      romanNumerals += key
+      num -= numsAndRomans[key]
+    }
+  })
+  return romanNumerals;
+}
