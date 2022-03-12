23cdab5b10b07b4f3de435f0a61bf89b3152d3e2
deedeeh <dinahafez.8@gmail.com>
2022-03-12 13:19:04 +0000

    Removed comment

--- src/roman_numerals.ts
+++ src/roman_numerals.ts
@@ -1,5 +1,4 @@
 export const numToRomanNumerals = (num: number): string => {
-  //hello world
   if(num < 0) throw new Error("A positive number is required");
   let romanNumerals = "";
   const numsAndRomans = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1};
